11077e1ae80d0aab8656adf4ff9d4efef3816316
Mohammed <mohammedamr94@gmail.com>
2015-12-28 18:43:30 +0200

    Fixed problem with merge

--- main/main.cpp
+++ main/main.cpp
@@ -76,56 +76,56 @@ int main( void )
     // Initialise GLFW
     srand (time(NULL));
     if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		getchar();
-		return -1;
-	}
-	glfwWindowHint(GLFW_SAMPLES, 4);
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        getchar();
+        return -1;
+    }
+    glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-	// Open a window and create its OpenGL context
+    // Open a window and create its OpenGL context
     window = glfwCreateWindow( 1024, 768, "Feeding Fawzy", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window.\n" );
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	// Initialize GLEW
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-	// Ensure we can capture the escape key being pressed below
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    if( window == NULL ){
+        fprintf( stderr, "Failed to open GLFW window.\n" );
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     // Hide the mouse and enable unlimited mouvement
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);   
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
     // Set the mouse at the center of the screen
     glfwPollEvents();
     glfwSetCursorPos(window, 1024/2, 768/2);
-	// Dark blue background
+    // Dark blue background
     glClearColor(0.176470588f, 0.443137255f, 0.768627451f, 0.0f);
-	// Enable depth test
-	glEnable(GL_DEPTH_TEST);
-	// Accept fragment if it closer to the camera than the former one
-	glDepthFunc(GL_LESS); 
-	// Cull triangles which normal is not towards the camera
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS);
+    // Cull triangles which normal is not towards the camera
     glEnable(GL_CULL_FACE);;
-	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader" );
-	// Get a handle for our "MVP" uniform
+    // Create and compile our GLSL program from the shaders
+    GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader" );
+    // Get a handle for our "MVP" uniform
     MatrixID = glGetUniformLocation(programID, "MVP");
-	// Get a handle for our buffers
+    // Get a handle for our buffers
 
-	GLuint vertexPosition_modelspaceID = glGetAttribLocation(programID, "vertexPosition_modelspace");
-	GLuint vertexUVID = glGetAttribLocation(programID, "vertexUV");
+    vertexPosition_modelspaceID = glGetAttribLocation(programID, "vertexPosition_modelspace");
+    vertexUVID = glGetAttribLocation(programID, "vertexUV");
     Obj::vertexNormID = glGetAttribLocation(programID,"vertexNormal_modelspace");
-	// Get a handle for our "myTextureSampler" uniform
-    GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
+    // Get a handle for our "myTextureSampler" uniform
+    TextureID  = glGetUniformLocation(programID, "myTextureSampler");
 
     Obj::ProgramID = programID;
     Obj::ViewMID = glGetUniformLocation(programID, "V");
@@ -198,10 +198,10 @@ int main( void )
 
     do{
 
-		// Clear the screen
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		// Use our shader
-		glUseProgram(programID);
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // Use our shader
+        glUseProgram(programID);
 
         glm::vec3 lightPos = glm::vec3(5, 10, -10);
         glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.z);
@@ -275,21 +275,21 @@ int main( void )
         fawzy->draw(ViewMatrix,ProjectionMatrix);
 
         glDisableVertexAttribArray(vertexPosition_modelspaceID);
-		glDisableVertexAttribArray(vertexUVID);
+        glDisableVertexAttribArray(vertexUVID);
         glDisableVertexAttribArray(Obj::vertexNormID);
 
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
 
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0 );
 
-	glDeleteProgram(programID);
+    glDeleteProgram(programID);
     glDeleteTextures(1, &TextureID);
-	// Close OpenGL window and terminate GLFW
-	glfwTerminate();
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
 
     return 0;
 }
